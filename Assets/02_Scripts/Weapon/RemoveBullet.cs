00353e5d2e5655a2b004de4401d589412f38a3fb
Lee JaeHyun <myleje@gmail.com>
2026-07-15 11:33:34 +0900

    .

--- Assets/02_Scripts/Weapon/RemoveBullet.cs
+++ Assets/02_Scripts/Weapon/RemoveBullet.cs
@@ -9,6 +9,18 @@ public class RemoveBullet : MonoBehaviour
         //if (collision.collider.tag == "BULLET") // Garbage Collection 발생하는 코드
         if (collision.collider.CompareTag("BULLET"))
         {
+            // 충돌 정보 추출
+            ContactPoint cp = collision.GetContact(0);
+            // 충돌 좌표 추출
+            Vector3 point = cp.point;
+            // 법선 벡터 추출
+            Vector3 normal = -cp.normal;
+            // 벡터를 쿼터니언 타입으로 변환
+            Quaternion rot = Quaternion.LookRotation(normal);
+
+            // 스파크 이펙트 생성
+            Instantiate(_sparkEffect, point, rot);
+
             Destroy(collision.gameObject);
         }
     }
