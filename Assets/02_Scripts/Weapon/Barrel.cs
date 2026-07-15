609d2526fad3bfca928e1d9bcd95573e54e3a3fd
Lee JaeHyun <myleje@gmail.com>
2026-07-15 13:32:23 +0900

    .

--- Assets/02_Scripts/Weapon/Barrel.cs
+++ Assets/02_Scripts/Weapon/Barrel.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Barrel : MonoBehaviour
 {
@@ -20,10 +21,14 @@ public class Barrel : MonoBehaviour
     private void ExpBarrel()
     {
         var rb = this.gameObject.AddComponent<Rigidbody>();
-        rb.AddForce(Vector3.up * 1500.0f);
+        // rb.AddForce(Vector3.up * 1500.0f);
+
+        Vector3 pos = Random.insideUnitSphere; // 랜덤한 x,y,z 리턴 (단위 구체)
+        rb.AddForceAtPosition(Vector3.up * 1500f, transform.position + pos);
 
         Destroy(this.gameObject, 3.0f);
 
-        Instantiate(_expEffect, transform.position, Quaternion.identity);
+        var obj = Instantiate(_expEffect, transform.position, Quaternion.identity);
+        Destroy(obj, 5.0f);
     }
 }
