a66b4bde9bb065aeebbbf56c06f71096d89c8939
Lee JaeHyun <myleje@gmail.com>
2026-07-15 11:45:28 +0900

    .

---
+++ Assets/02_Scripts/Weapon/Barrel.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    private int _hitCount = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("BULLET"))
+        {
+            if (++_hitCount >= 3)
+            {
+                ExpBarrel();
+            }
+        }
+    }
+
+    private void ExpBarrel()
+    {
+        var rb = this.gameObject.AddComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * 1000.0f);
+    }
+}
