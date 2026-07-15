cfad5a43841c78b61a970faa9546451f3a5b673e
Lee JaeHyun <myleje@gmail.com>
2026-07-15 10:35:09 +0900

    .

--- Assets/02_Scripts/Weapon/Fire.cs
+++ Assets/02_Scripts/Weapon/Fire.cs
@@ -8,6 +8,13 @@ public class Fire : MonoBehaviour
     [SerializeField] private List<AudioClip> _fireSfx;
     public int currentWeapon = 0;
 
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -15,6 +22,10 @@ public class Fire : MonoBehaviour
             // 총알 생성
             // Instantiate(생성할객체, 위치, 각도)
             Instantiate(_bulletPrefab, _firePos.position, _firePos.rotation);
+            // 사운드 재생
+            // _audioSource.Play(); // BGM
+            // 연속 사운드 재생
+            _audioSource.PlayOneShot(_fireSfx[currentWeapon]);
         }
     }
 }
