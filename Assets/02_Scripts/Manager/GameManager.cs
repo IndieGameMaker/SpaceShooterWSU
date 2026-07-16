74b2a28869abd997e2e42817b800cbe31007fd74
Lee JaeHyun <myleje@gmail.com>
2026-07-16 16:38:17 +0900

    Update GameManager.cs

--- Assets/02_Scripts/Manager/GameManager.cs
+++ Assets/02_Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,4 +7,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float _spawnTime = 3.0f;
     // 생성할 몬스터 프리팹
     [SerializeField] private GameObject _monsterPrefab;
+    // Spawn Point 배열
+    [SerializeField] private List<Transform> _points = new List<Transform>();
+    // [SerializeField] private List<Transform> _points = new ();
+
+    private void Start()
+    {
+        GameObject.Find("SpawnPointGroup")?.GetComponentsInChildren<Transform>(_points);
+    }
+
 }
