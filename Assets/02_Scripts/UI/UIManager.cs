1c22c8cf84cd1f7666ede3a2a9c01b2cb39e8526
Lee JaeHyun <myleje@gmail.com>
2026-07-21 10:16:09 +0900

    Update UIManager.cs

--- Assets/02_Scripts/UI/UIManager.cs
+++ Assets/02_Scripts/UI/UIManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("UI Buttons")]
+    [SerializeField] private Button _startButton;
+
     public void OnStartButtonClick()
     {
         // ¾À ·ÎµùÃ³¸®
